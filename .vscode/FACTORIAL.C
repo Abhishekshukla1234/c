bef320cc9ae724c4b40c976177d7cf858c161397
abhishek <shuklaabhishekkumar71@gmail.com>
2022-10-22 12:55:18 +0530

    factorial
    Signed-off-by: abhishek <shuklaabhishekkumar71@gmail.com>

---
+++ .vscode/FACTORIAL.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int fact (int n);
+int main()
+{ int n,N;
+printf("enter the value:");
+scanf("%d",&n);
+    printf("factorial is : %d",fact(n));
+    return 0;
+}
+int fact(int n)
+{
+    if(n==0)
+ {
+    return 1;
+ }   
+int factNm1 = fact(n-1);
+int factN=fact(n-1) * n;
+return factN;
+}
