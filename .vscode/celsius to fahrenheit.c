265e411eff149d7217a467685ca37e468228e970
abhishek <shuklaabhishekkumar71@gmail.com>
2022-10-25 21:12:21 +0530

    SUM OF N NUMBER USING  RECURSION
    Signed-off-by: abhishek <shuklaabhishekkumar71@gmail.com>

---
+++ .vscode/celsius to fahrenheit.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+float convert temp(flaot celsius)
+int main()
+{
+    float far=canvert temp(37);
+    printf("far: %f", far);
+    return 0;
+}
+float convert temp(float celsius)
+{
+    flooat far == celsius *(9.0/5.0)+32 ;
+    return far;
+}
