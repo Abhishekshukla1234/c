fcd4a1f73bc20714c8b9b56e30effc93686d9cf7
Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>
2022-12-04 09:55:32 +0530

    reading from a file
    Signed-off-by: Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>

---
+++ structure/file/readingfromafile.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+{
+    FILE*fptr;
+    fptr=fopen("text.txt","r");
+    char ch;
+    fscanf(fptr,"%c",&ch);
+    printf("character=%c",ch);
+    fclose(fptr);
+    return 0;
+}
